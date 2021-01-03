64aa8144b7019288f53d5582845be1b822797a6f
Sarathkrishnan <sarathkrishnangs@gmail.com>
2021-01-03 19:35:31 +0530

    Day 2 Assignment : template and views urls added

--- festsite/events/models.py
+++ festsite/events/models.py
@@ -1,18 +1,34 @@
 from django.db import models
+import datetime
+import django.utils
 # from datetime import datetime
 # from django.utils import timezone
 
 # Create your models here.
+
+
 class Events(models.Model):
-    id = models.IntegerField(primary_key=True)
+
     name = models.CharField(max_length=20)
-    details = models.CharField(max_length=200,default="")
-    #date = models.DateField
-    #time = models.TimeField
-    fees = models.IntegerField
+    details = models.CharField(max_length=200, default="")
+
+    #programme details
+    event_banner= models.ImageField(upload_to=None,default=None)
+    no_of_seats = models.FloatField(default=0)
+    date = models.DateField(default=django.utils.timezone.now)
+    time = models.TimeField(default=django.utils.timezone.now)
+    fees = models.FloatField(default=0)
     contact = models.CharField(max_length=15)
 
-    
+    #Price Details
+    first_price = models.FloatField(default=0)
+    second_price = models.FloatField(default=0)
+    third_price = models.FloatField(default=0)
+
+    #Group programmes's details
+    is_Group = models.BooleanField(default=True)
+    max_GroupMembers = models.IntegerField(default=1)
+    min_GroupMembers = models.IntegerField(default=1)
+
     def __str__(self):
         return self.name
-
--- festsite/festsite/urls.py
+++ festsite/festsite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from events.views import home, event
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+   # path('event/', event, name='event'),
+    path('event/<int:event_id>',event, name='event')
 ]
